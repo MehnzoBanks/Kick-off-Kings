f77285ec8ee509664a0d8d9b68b24942f6634b2e
MehnzoBanks <mensahdavid814@gmail.com>
2026-09-26 10:34:01 +0100

    Add HTTP and JSON imports to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
 
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 void main() => runApp(const KickoffKingsApp());
 
 class KickoffKingsApp extends StatelessWidget {
